a33ef0d516de6efc2fedfe1f739f04c2c0a1e9c6
Md. Abdullah Al Mehedi <abdullahalmehedi86@gmail.com>
2026-03-11 13:36:08 +0600

    organizing routed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,13 @@
+import cors from "cors"
 import express, { type Request, type Response } from "express"
+import { router } from "./routes/index.js";
 
 const app = express();
+
+app.use(express.json())
+app.use(cors())
+
+app.use("/api/v1", router)
 
 app.get("/", (req: Request, res: Response) => {
     res.status(200).json({
