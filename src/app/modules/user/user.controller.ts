eb47e79520f11008f645be3368887ab7832075c5
Md. Abdullah Al Mehedi <abdullahalmehedi86@gmail.com>
2026-03-14 17:43:20 +0600

    added: response utility function

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,25 +2,31 @@ import type { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes"
 import { userServices } from "./user.service.js";
 import catchAsync from "../../utils/catchAsync.js";
+import { sendResponse } from "../../utils/sendResponse.js";
 
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const createUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
 
     const user = await userServices.createUser(req.body);
-    res.status(httpStatus.CREATED).json({
-        message: "User created successfully!",
-        user
+    sendResponse(res, {
+        success: true,
+        statusCode: httpStatus.OK,
+        message: "User created successfully!!",
+        data: user
     })
 })
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const getAllUsers = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const users = await userServices.getAllUsers();
-    res.status(httpStatus.OK).json({
+    const result = await userServices.getAllUsers();
+
+    sendResponse(res, {
         success: true,
+        statusCode: httpStatus.OK,
         message: "All users retrived successfully!!",
-        data: users
+        data: result.data,
+        meta: result.meta
     })
 })
 
