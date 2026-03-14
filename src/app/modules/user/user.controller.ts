dec3f4502aa7e24f26542e8eba82b12c8a77de47
Md. Abdullah Al Mehedi <abdullahalmehedi86@gmail.com>
2026-03-14 14:30:21 +0600

    added: reduce repeatation of try catch block

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,22 +1,30 @@
 import type { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes"
 import { userServices } from "./user.service.js";
+import catchAsync from "../../utils/catchAsync.js";
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const user = await userServices.createUser(req.body);
 
-        res.status(httpStatus.CREATED).json({
-            message: "User created successfully!",
-            user
-        })
-    } catch (error: unknown) {
-        // eslint-disable-next-line no-console
-        console.log(error);
-        next(error)
-    }
-}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const createUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+
+    const user = await userServices.createUser(req.body);
+    res.status(httpStatus.CREATED).json({
+        message: "User created successfully!",
+        user
+    })
+})
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const getAllUsers = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const users = await userServices.getAllUsers();
+    res.status(httpStatus.OK).json({
+        success: true,
+        message: "All users retrived successfully!!",
+        data: users
+    })
+})
 
 export const userControllers = {
-    createUser
+    createUser,
+    getAllUsers
 }
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -11,6 +11,13 @@ const createUser = async (payload: Partial<IUser>) => {
     return user;
 }
 
+const getAllUsers = async () => {
+    const users = await User.find({});
+
+    return users;
+}
+
 export const userServices = {
-    createUser
+    createUser,
+    getAllUsers
 }
