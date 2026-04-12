ecdba10ee49d8af1968834c55988b5ba0e2c602c
Md. Abdullah Al Mehedi <abdullahalmehedi86@gmail.com>
2026-04-12 16:22:43 +0600

    added: tour, tour type and division shema and model

---
+++ src/app/modules/division/division.interface.ts
@@ -0,0 +1,6 @@
+export interface IDivision {
+    name: string;
+    slug: string; // Chattogram Division  er slug hocche chattogram-division /:slug er jonno
+    thumbnail?: string;
+    description?: string;
+}
--- src/app/utils/catchAsync.ts
+++ src/app/utils/catchAsync.ts
@@ -1,4 +1,5 @@
 import type { NextFunction, Request, Response } from "express";
+import { envVars } from "../config/env.js";
 
 type AsyncHandler = (req: Request, res: Response, next: NextFunction) => Promise<void>;
 
@@ -6,7 +7,6 @@ type AsyncHandler = (req: Request, res: Response, next: NextFunction) => Promise
 // alternative of try catch to avoid repeatative try catch writings
 const catchAsync = (fn: AsyncHandler) => (req: Request, res: Response, next: NextFunction) => {
     Promise.resolve(fn(req, res, next)).catch((err: unknown) => {
-        console.log(err);
         next(err);
     })
 }
