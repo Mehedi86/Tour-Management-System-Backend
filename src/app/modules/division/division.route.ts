1e4eae0e787951e287945205d8d53cfcb0606dd6
Md. Abdullah Al Mehedi <abdullahalmehedi86@gmail.com>
2026-04-13 16:11:20 +0600

    added: tour and tour-type controller and service

---
+++ src/app/modules/division/division.route.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import { checkAuth } from "../../middlewares/checkAuth.js";
+import { createDivisionSchema, updateDivisionSchema } from "./division.validation.js";
+import { validateRequest } from "../../middlewares/validateRequest.js";
+import { DivisionController } from "./division.controller.js";
+import { Role } from "../user/user.interface.js";
+
+const router = Router()
+
+router.post("/create", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), validateRequest(createDivisionSchema),
+    DivisionController.createDivision
+);
+
+router.get("/", DivisionController.getAllDivisions);
+
+router.get("/:slug", DivisionController.getSingleDivision)
+
+router.patch("/:id", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), validateRequest(updateDivisionSchema),
+    DivisionController.updateDivision
+);
+
+router.delete("/:id", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), DivisionController.deleteDivision);
+
+export const DivisionRoutes = router
