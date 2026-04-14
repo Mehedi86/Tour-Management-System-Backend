8f5d42e4baebbe79a80299ef4c1c87a3b6b9d701
Md. Abdullah Al Mehedi <abdullahalmehedi86@gmail.com>
2026-04-14 11:50:20 +0600

    added: slug pre hook

--- src/app/modules/tour/tour.service.ts
+++ src/app/modules/tour/tour.service.ts
@@ -9,16 +9,6 @@ const createTour = async (payload: ITour) => {
         throw new Error("A tour with this title already exists.");
     }
 
-    const baseSlug = payload.title.toLowerCase().split(" ").join("-")
-    let slug = `${baseSlug}`
-
-    let counter = 0;
-    while (await Tour.exists({ slug })) {
-        slug = `${slug}-${counter++}` // dhaka-division-2
-    }
-
-    payload.slug = slug;
-
     const tour = await Tour.create(payload)
 
     return tour;
@@ -55,18 +45,6 @@ const updateTour = async (id: string, payload: Partial<ITour>) => {
 
     if (!existingTour) {
         throw new Error("Tour not found.");
-    }
-
-    if (payload.title) {
-        const baseSlug = payload.title.toLowerCase().split(" ").join("-")
-        let slug = `${baseSlug}`
-
-        let counter = 0;
-        while (await Tour.exists({ slug })) {
-            slug = `${slug}-${counter++}` // dhaka-division-2
-        }
-
-        payload.slug = slug
     }
 
     const updatedTour = await Tour.findByIdAndUpdate(id, payload, { new: true });
