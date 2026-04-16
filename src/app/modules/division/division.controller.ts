3daf9ab8c110d9961af367ca70af9f697d9d2539
Md. Abdullah Al Mehedi <abdullahalmehedi86@gmail.com>
2026-04-16 19:34:12 +0600

    added: some change in tour schema

--- src/app/modules/division/division.controller.ts
+++ src/app/modules/division/division.controller.ts
@@ -25,6 +25,7 @@ const getAllDivisions = catchAsync(async (req: Request, res: Response) => {
         meta: result.meta,
     });
 });
+
 const getSingleDivision = catchAsync(async (req: Request, res: Response) => {
     const slug = req.params.slug;
 
