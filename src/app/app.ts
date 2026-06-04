55fdd37757490efd5be1b4f73a670c58d608b095
Md. Abdullah Al Mehedi <abdullahalmehedi86@gmail.com>
2026-06-04 13:40:29 +0600

    added: cloudinary single image upload

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,38 +1,40 @@
-import cors from "cors"
-import express, { type Request, type Response } from "express"
+import cors from "cors";
+import express, { type Request, type Response } from "express";
 import "./config/passport.js";
 import { router } from "./routes/index.js";
 import { globalErrorHandler } from "./middlewares/globalErrorHandler.js";
 import notFound from "./middlewares/notFound.js";
 import cookieParser from "cookie-parser";
 import passport from "passport";
-import expressSession from "express-session"
-
+import expressSession from "express-session";
 
 const app = express();
 
-app.use(expressSession({
+app.use(
+  expressSession({
     secret: "your secret",
     resave: false,
-    saveUninitialized: false
-}))
+    saveUninitialized: false,
+  }),
+);
 
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(cookieParser())
-app.use(express.json())
-app.use(cors())
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-app.use("/api/v1", router)
+app.use("/api/v1", router);
 
 app.get("/", (req: Request, res: Response) => {
-    res.status(200).json({
-        message: "welcome tour-management-system backend"
-    })
-})
+  res.status(200).json({
+    message: "welcome tour-management-system backend",
+  });
+});
 
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
-app.use(notFound)
+app.use(notFound);
 
 export default app;
--- src/app/modules/booking/booking.service.ts
+++ src/app/modules/booking/booking.service.ts
@@ -89,7 +89,6 @@ const createBooking = async (payload: Partial<IBooking>, userId: string) => {
         }
 
         const sslPayment = await SSLService.sslPaymentInit(sslPayload);
-        console.log(sslPayment)
 
         await session.commitTransaction();
         session.endSession();
